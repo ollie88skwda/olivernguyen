58537fd3dfc6a26ec96e0fe6c591ead44264689e
Oliver <oliverdnguyen@gmail.com>
2024-06-22 18:04:22 -0700

    hi

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <p>hello</p>
+      <header className="App-header">
+        <p>hi</p>
+      </header>
     </div>
   );
 }
