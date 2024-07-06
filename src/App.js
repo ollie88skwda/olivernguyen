6e05c86422d0044a7a157d2e500a81e8b62d9651
Oliver <oliverdnguyen@gmail.com>
2024-07-05 23:38:57 -0700

    test

--- src/App.js
+++ src/App.js
@@ -28,7 +28,6 @@ function App() {
 
   return (
     <div>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       <div class="content">
         <main>
           <header>
