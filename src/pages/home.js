f15dba13d1c0b1248004520b4d0eea36067d8eac
Oliver <oliverdnguyen@gmail.com>
2024-08-06 01:13:30 -0700

    slightly change front page... took an hour

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,15 @@
-import {TopBar} from "./top_bar";
+import { TopBar } from "./top_bar";
+import "../Home.css";
+import Front_Page_Text from "../assets/front_page_text.png";
 export const Home = () => {
   return (
-    <div class="content">
-      <h1>HOME</h1>
+    <div class="home-page-content">
+      <img
+        src={Front_Page_Text}
+        alt="OLIVER NGUYEN"
+        height="1000px"
+        class="home-title-text"
+      />
       <h1>
         <a href="/driving">Learn to drive</a>
       </h1>
