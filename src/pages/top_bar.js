d2b01bdd5ee7d65f295087311e995396f51fd1e6
Oliver <oliverdnguyen@gmail.com>
2024-08-26 17:03:09 -0700

    added a side bar

--- src/pages/top_bar.js
+++ src/pages/top_bar.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect, useCallback } from "react";
 import on_logo from "../assets/on_logo.png";
 import "../Top_Bar.css";
+
 export const TopBar = () => {
   const [showTopBar, setShowTopBar] = useState(true);
   const [lastScrollTop, setLastScrollTop] = useState(0);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false); // Sidebar state
   const SCROLL_TOP_THRESHOLD = 25; // Adjust this value to set the threshold near the top
 
   const handleScroll = useCallback(() => {
@@ -27,18 +29,43 @@ export const TopBar = () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, [handleScroll]);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
-    <div class>
+    <div>
       <div className={`top-bar ${showTopBar ? "visible" : "hidden"}`}>
         <a href="../">
-          <img src={on_logo} width="100px" alt="website logo" class="on-logo"/>
+          <img
+            src={on_logo}
+            width="100px"
+            alt="website logo"
+            className="on-logo"
+          />
         </a>
-        <div class="dropdown">
-          <button>Projects</button>
-          <div class="dropdown-options">
-            <a href="/driving">Driving</a>
-          </div>
-        </div>
+        <button onClick={toggleSidebar} className="sidebar-toggle-btn">
+          ☰ {/* Sidebar toggle icon */}
+        </button>
+      </div>
+      <div className={`sidebar ${isSidebarOpen ? "open" : ""}`}>
+        <button onClick={toggleSidebar} className="close-btn">
+          &times; {/* Close icon */}
+        </button>
+        {/* Sidebar content */}
+        <ul>
+          <li>
+            <a href="/" className="sidebar-link">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="/driving" className="sidebar-link">
+              Driving
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   );
