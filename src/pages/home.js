03d22b2e4941a50843cc1c1db80e7e9a37c0f6e1
ollie88skwda <oliverdnguyen@gmail.com>
2025-01-19 22:15:16 -0800

    a lot of animation stuff

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { TopBar } from "./top_bar";
 import "../Home.css";
+import { motion } from "framer-motion";
 
 export const Home = () => {
   useEffect(() => {
@@ -16,8 +17,47 @@ export const Home = () => {
       <div className="intro-section">
         <p className="greeting animate-in">Hi, my name is</p>
         <h1 className="name animate-in">Oliver Nguyen.</h1>
-        {/* <h2 className="description animate-in">I love</h2> */}
+        <h2 className="description animate-in">
+          I love to <a>build </a>things.
+        </h2>
+        <div style={{ height: "50vh" }}></div>
       </div>
+      <main>
+        <div className="definition">
+          <motion.div
+            initial={{ opacity: 0, y: -20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8 }}
+          >
+            <h1>build</h1>
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, y: -20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, delay: 0.2 }}
+          >
+            <h2
+              className="pronunciation"
+              style={{ fontFamily: "GeistMono", fontSize: "1.5rem" }}
+            >
+              /bild/ • verb
+            </h2>
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, y: -20 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8, delay: 0.2 }}
+          >
+            <h2
+              className="definition-text"
+              style={{ fontFamily: "GeistMono", fontSize: "1.5rem" }}
+            >
+              to develop according to a systematic plan, by a definite process,
+              or on a particular base
+            </h2>
+          </motion.div>
+        </div>
+      </main>
       <TopBar />
     </div>
   );
